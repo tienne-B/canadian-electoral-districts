f74fe76be09cc1623146f709f4ea9be53cdbf8b3
Étienne Beaulé <beauleetienne0@gmail.com>
2015-12-23 12:17:58 -0400

    Add script
    Add script used to split files into districts.

---
+++ scripts/split.rb
@@ -0,0 +1,22 @@
+#! /usr/bin/ruby
+# Script to separate geojson collection to a file for each district.
+require 'rubygems'
+require 'fileutils'
+require 'json'
+
+ROOT = File.expand_path("../", File.dirname(__FILE__))
+year = 2013
+
+def write(destination, data)
+  File.open(destination, "w") { |file| file.write data.to_json }
+end
+
+# Parse JSON and split each district into its own file
+data = JSON.parse File.open("#{year}.geojson").read
+
+data["features"].each do |district|
+  ridingID = district["properties"]["FEDNUM"]
+  puts "Processing #{ridingID}..."
+  file = "#{year}/#{ridingID}.geojson"
+  write(file, district)
+end
